723dee2648ce8bc3ee77b0d93e6a3225392b9720
Antonio Blandin <antonio.blandin25@gmail.com>
2019-11-18 18:00:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index[""]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[0] = 
+  array[0] = "2"
 end
